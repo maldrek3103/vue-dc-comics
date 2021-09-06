6c05ae3e049535d3212bff9970b4c006d60cbb24
maldrek3103 <cpisanu96@gmail.com>
2021-09-06 16:22:26 +0200

    started header, added imgs

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <section id="header">
+    <img src="../assets/img/dc-logo.png" alt="Logo" />
+    <ul id="navbar">
+      <li><a href="#">Characters</a></li>
+      <li><a href="#">Comics</a></li>
+      <li><a href="#">Movies</a></li>
+      <li><a href="#">TV</a></li>
+      <li><a href="#">Games</a></li>
+      <li><a href="#">Collectibles</a></li>
+      <li><a href="#">Videos</a></li>
+      <li><a href="#">Fans</a></li>
+      <li><a href="#">News</a></li>
+      <li><a href="#">Shop</a></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+#header img {
+  height: 100%;
+  width: auto;
+}
+</style>
